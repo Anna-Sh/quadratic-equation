64b8c547973188617e1fea481c4ac2a0c5f702d7
Anna-Sh <a.shmidova@yandex.ru>
2018-09-25 19:25:12 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    //парсим входящее уравнение
+    const equationRegex = /^(-?\d+)\s\*\sx\^2\s([+-]\s\d+)\s\*\sx\s([+-]\s\d+)$/i;
+
+    let groups = equationRegex.exec(equation);
+
+    //D=B^2-4*A*C
+    let a = groups[1],
+        b = groups[2],
+        c = groups[3];
+
+    b =  b.replace(" ", "");
+    c =  c.replace(" ", "");
+
+    a = parseInt(a, 10);
+    b = parseInt(b, 10);
+    c = parseInt(c, 10);
+
+    const d = Math.pow(b, 2)-4*a*c,
+        x1 = Math.round((-b + Math.sqrt(d)) / (2*a)),
+        x2 = Math.round((-b - Math.sqrt(d)) / (2*a));
+
+    const result = [x1, x2];
+
+    result.sort( (a, b) => a - b );
+
+    return result;
+};
